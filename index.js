f2570c68211c8290995320da2ae181468d53d98e
Kate Popova <popova1ea@gmail.com>
2022-11-22 21:46:11 -0800

    commit tree aca2fe984babc12aec56b51e6376a1520d2a134e

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+if (someValue <= 400){
+  return 'This one is on me!'}
+else if (someValue > 400, someValue <=2000) {
+  return "That will be twenty bucks."}
+else if(someValue > 2000, someValue <= 2500) {
+  return "I will gladly take your thirty bucks." }
+else if (someValue > 2500) {
+  return "No can do."}
 }
 
-function ternaryCheckCity(){
+
+function ternaryCheckCity(City){
   // Write your code here!
+return City === "NYC" ? "Ok, sounds good.":"No go."
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(Tip){
   // Write your code here!
+  switch (Tip) {
+    case "generous":
+      return "Thank you so much."
+    case "not as generous":  
+      return "Thank you."
+      default :
+      return "Bye."
+  }
 }
